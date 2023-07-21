9823fce88876294a8ca134e93c018bdf75f34e2f
Thanakorn Boonyasena <121655106+ThanakornBoonyasena@users.noreply.github.com>
2023-07-21 13:55:49 +0700

    dropdown

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-function rotate(x,y) {
+const rotate = (degs) => {
+    const deglist = degs.split(",");
+    const x = deglist[0];
+    const y = deglist[1];
     const cube = document.getElementById("cube");
     cube.style.setProperty("--cube-rotation", `rotateX(${x}deg) rotateY(${y}deg)`);
+    console.log("x: ", x, "y: ", y);
 }
